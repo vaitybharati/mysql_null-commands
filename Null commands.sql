7716833b26b9cee5ce70874082c67351116aa478
DSBK <71163471+vaitybharati@users.noreply.github.com>
2020-10-08 20:02:48 +0530

    Add files via upload

---
+++ Null commands.sql
@@ -0,0 +1,13 @@
+#### Null commands
+select * from students;
+select "hello";
+select 5+7;
+select 5+7 as result;
+select 6=6;
+select 6=5;
+select 3=null;
+select 3 is null;
+select 3 is not null;
+select * from students;
+
+
